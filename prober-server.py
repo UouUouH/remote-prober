71bcedbeacd729ad739a899a77bebfb297af8060
na <nayang2011@gmail.com>
2015-01-28 01:29:08 -0800

    modified prober-server.py

--- prober-server.py
+++ prober-server.py
@@ -1,21 +1,48 @@
-#2015.1.26
+#2015.1.27
 
 from twisted.internet.defer import Deferred
 from twisted.internet.protocol import ServerFactory, Protocol
 from twisted.python import log
-import sys
+import sys, time
 
 class ProberProtocol(Protocol):
 
     def connectionMade(self):
         log.msg("connection made")
+        self.deferred = None
+
+    def processData(self, data):
+        def canceler(d):
+            log.msg("cancelling deffered")
+
+        def cancelerErr(err):
+            log.msg("err: %s" %(err,))
+            pass
+        
+        if self.deferred is not None:
+            log.msg("is not none")
+            deferred, self.deferred = self.deferred, None
+            deferred.cancel()
+        
+        log.msg("new deferred")
+        self.deferred = Deferred(canceler)
+        #self.deferred.addErrback(cancelerErr)
+        self.deferred.addCallbacks(self.factory.service.receiveCommand, cancelerErr)
+        self.deferred.addCallback(self.factory.service.processCommand)
+        self.deferred.addCallback(self.transport.write)
+
+        self.deferred.callback(data)
 
     def conncetionLost(self, reason):
         log.msg("connection lost for %s" %(reason,))
+        if self.deffered is not None:
+            deferred, self.deferred = self.deferred, None
+            deferred.cancel()
 
     def dataReceived(self, data):
         log.msg("recived data: %s" %(data,))
-        self.factory.service.receiveCommand(data)
+        #self.factory.service.receiveCommand(data)
+        self.processData(data)
 
 
 class ProberFactory(ServerFactory):
@@ -29,17 +56,23 @@ class ProberService(object):
 
     commandList = []
 
-    def receiveCommand(self,command):
+    def receiveCommand(self, command):
         self.commandList.append(command)
 
-    def processCommand():
+    def processCommand(self, res):
         processedCommand = None
         if len(self.commandList) > 0:
             processedCommand = self.commandList[0]
             del self.commandList[0]
-        return processedCommand
+        log.msg("processedCommand %s" %(processedCommand,))
+        return'rec:'+ processedCommand
+    
+    def getPosition():
+        return "fake position"
+    
 
 def main():
+
     service = ProberService()
     factory = ProberFactory(service)
 
@@ -51,7 +84,7 @@ def main():
     port = reactor.listenTCP(1000, factory, interface = 'localhost')
 
     print 'prober server on %s:%s' %(port.getHost(), port)
-
+    
     reactor.run()
 
 if __name__ == '__main__':
