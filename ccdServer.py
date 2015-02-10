2fe9e08058cff42b59f44477f21f07fef8043d69
na <nayang2011@gmail.com>
2015-02-10 01:48:00 -0800

    update ccdServer.py

--- ccdServer.py
+++ ccdServer.py
@@ -18,18 +18,23 @@ class CCDProtocol(DatagramProtol):
         print "no one listening"
 
 
-class CCDService(object):
+class CCDService(object)
 
-    frameList = []
+    frameDic= {}
     def __int__(self, protocol):
         self.protocol = protocol
-        self.frameSize = 0
         self.frameWidth = 667
         self.frameHeight = 515
+        self.frameSize = (self.frameWidth*3+3)/4*4*self.frameHeight 
         self.packageMaxSize = 60007
         self.packageHeadSize = 7
-        self.lastPackageSize = 20
-        self.packageNum = 20
+        self.lastPackageSize = self.frameSize%self.packageMaxSize + self.packageHeadSize
+        if self.lastPackageSize > self.packageHeadSize 
+            self.pacakgeNum = int(self.frameSize/self.packageMaxSize)+1
+        else:
+            self.pacakgeNum = int(self.frameSize/self.packageMaxSize)
+        #first the value of id1 is 0, then if it is more than 255,set it as 1
+        self.id1 = 0
 
 
     def getFrameInfo(self):
@@ -37,7 +42,7 @@ class CCDService(object):
         def trans(data, lenth):
             for i in range(0, length):
                 n = 0xff&(m>>8*i)
-                frameInfo.append(n)
+                frameInfo.append(chr(n))
 
         #1 frame size
         frameInfo.append('F')
@@ -61,15 +66,11 @@ class CCDService(object):
         str1 = ''.join(frameInfo)
         return str1
 
-
-
-
-
-
-
-
-    def getNewFrame(self, frame):
-        self.frameList.appen(frame)
+    def accpetNewFrame (self, frame):
+        if self.id1 == 256:
+            self.id1 = 1
+        self.frameDic[self.id1] = frame
+        self.id1 = self.id1 + 1
 
     def parseCommand(self, res):
         if res is not None:
@@ -85,5 +86,21 @@ class CCDService(object):
         return "err command"
 
     def sendFrameInfo(self):
+        self.protocol.transport.write(getFremeInfo())
+
+    def sendFrame(self):
+        id1 = 0
+        id2 =0#the max value is 255
+        def getPackageInfo():
+            return 'ID'+chr(0xff)+chr(0xff&id1)+chr(0xff)+chr(0xff&id2)+chr(0xff)
+
+        def sendPackage():
+            pac = frameList[0][
+            self.protocol.transport.write
+
+
+
+
+
 
 
